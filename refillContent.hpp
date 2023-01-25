c0848e5f63037db76cfe3e7f2bc5a36b0d752a65
Elia Albaba <ea152@uakron.edu>
2023-01-24 20:11:45 -0500

    Add refillContent() declaration

--- refillContent.hpp
+++ refillContent.hpp
@@ -1,5 +1,14 @@
 #ifndef INCLUDED_REFILLCONTENT_HPP
 #define INCLUDED_REFILLCONTENT_HPP
 
+/*
+    Refill the content preserving the existing data.
+
+    @param[in, out] content View of the content
+    @return Number of bytes read
+    @retval 0 EOF
+    @retval -1 Read error
+*/
+[[nodiscard]] int refillContent(std::string_view& content);
 
 #endif
