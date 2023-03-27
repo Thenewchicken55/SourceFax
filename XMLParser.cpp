32f45cf63078657cd773ba214754b9a893bb3226
Elia Albaba <ea152@uakron.edu>
2023-03-27 17:29:09 -0400

    Pass std::string_view as IN parameter

--- XMLParser.cpp
+++ XMLParser.cpp
@@ -41,16 +41,16 @@ XMLParser::XMLParser(std::string_view content)
 
 // parse XML
 void XMLParser::parse(  std::function<void()> startDocumentHandler,
-                        std::function<void(std::string_view& version, std::optional<std::string_view>& encoding, std::optional<std::string_view>& standalone)> XMLDeclarationHandler,
-                        std::function<void(std::string_view& qName, std::string_view& prefix, std::string_view& localName)> startTagHandler,
-                        std::function<void(std::string_view& qName, std::string_view& prefix, std::string_view& localName)> endTagHandler,
-                        std::function<void(std::string_view& characters)> characterEntityReferencesHandler,
-                        std::function<void(std::string_view& characters)> characterNonEntityReferencesHandler,
-                        std::function<void(std::string_view& qName, std::string_view& prefix, std::string_view& localName, std::string_view& value)> attributeHandler,
-                        std::function<void(std::string_view& prefix, std::string_view& uri)> XMLNamespaceHandler,
-                        std::function<void(std::string_view& value)> XMLCommentHandler,
-                        std::function<void(std::string_view& characters)> CDATAHandler,
-                        std::function<void(std::string_view& target, std::string_view& data)> processingInstructionHandler,
+                        std::function<void(std::string_view version, std::optional<std::string_view>& encoding, std::optional<std::string_view>& standalone)> XMLDeclarationHandler,
+                        std::function<void(std::string_view qName, std::string_view prefix, std::string_view localName)> startTagHandler,
+                        std::function<void(std::string_view qName, std::string_view prefix, std::string_view localName)> endTagHandler,
+                        std::function<void(std::string_view characters)> characterEntityReferencesHandler,
+                        std::function<void(std::string_view characters)> characterNonEntityReferencesHandler,
+                        std::function<void(std::string_view qName, std::string_view prefix, std::string_view localName, std::string_view value)> attributeHandler,
+                        std::function<void(std::string_view prefix, std::string_view uri)> XMLNamespaceHandler,
+                        std::function<void(std::string_view value)> XMLCommentHandler,
+                        std::function<void(std::string_view characters)> CDATAHandler,
+                        std::function<void(std::string_view target, std::string_view data)> processingInstructionHandler,
                         std::function<void()> endDocumentHandler
                         ) {
 
