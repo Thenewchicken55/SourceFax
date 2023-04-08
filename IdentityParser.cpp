c1fe22b77c19bf0cf25e094e28753d76e67497b1
Elia Albaba <ea152@uakron.edu>
2023-04-08 17:30:35 -0400

    Fix identity to use identityParser

--- IdentityParser.cpp
+++ IdentityParser.cpp
@@ -3,3 +3,116 @@
 
     Concrete class specific to Identity inheriting from the abstract class XMLParser
 */
+
+#include "IdentityParser.hpp"
+#include <iostream>
+
+int unclosedBrackets = 0;
+
+// provides literal string operator""sv
+using namespace std::literals::string_view_literals;
+
+std::string escape(std::string_view unescaped) {
+    std::string unescapedString(unescaped);
+    std::string escapedString = "";
+    for(auto c: unescapedString) {
+        if(c == '<') {
+            escapedString += "&lt;";
+        } else if (c == '>') {
+            escapedString += "&gt;";
+        } else if (c == '&') {
+            escapedString += "&amp;";
+        } else {
+            escapedString += c;
+        }
+    }
+    return escapedString;
+}
+
+// constructor
+IdentityParser::IdentityParser(std::string_view content)
+    : XMLParser(content)
+    {}
+
+//get loc
+int IdentityParser::getLoc() {
+    return loc;
+}
+
+// start Document Handler
+void IdentityParser::handleStartDocument() {}
+
+// XML Declaration Handler
+void IdentityParser::handleXMLDeclaration(std::string_view version, std::optional<std::string_view>& encoding, std::optional<std::string_view>& standalone) {
+    std::cout << "<?xml version=\"" << version << "\" ";
+
+    if (encoding.has_value()) {
+        std::cout << "encoding=\"" << encoding.value() << "\" ";
+    }
+
+    if (standalone.has_value()) {
+        std::cout << "standalone=\"" << standalone.value() << "\"";
+    }
+
+    std::cout << "?>" << std::endl;
+}
+
+// Start Tag Handler
+void IdentityParser::handleStartTag(std::string_view qName, std::string_view prefix, std::string_view localName) {
+    if (unclosedBrackets > 0) {
+            --unclosedBrackets;
+            std::cout << ">";
+    }
+    std::cout << "<" << qName;
+    ++unclosedBrackets;
+}
+
+// End Tag Handler
+void IdentityParser::handleEndTag(std::string_view qName, std::string_view prefix, std::string_view localName) {
+    std::cout << "</" <<  qName << ">";
+}
+
+// Character Handler
+void IdentityParser::handleCharacter(std::string_view characters) {
+    if (unclosedBrackets > 0) {
+            --unclosedBrackets;
+            std::cout << ">";
+    }
+    std::cout << escape(characters);
+
+    loc += static_cast<int>(std::count(characters.cbegin(), characters.cend(), '\n'));
+}
+
+// attribute Handler
+void IdentityParser::handleAttribute(std::string_view qName, std::string_view prefix, std::string_view localName, std::string_view value) {
+    std::cout << " " << qName << "=\"" << value << "\"";
+}
+
+// XML Namespace Handler
+void IdentityParser::handleXMLNamespace(std::string_view prefix, std::string_view uri) {
+    std::cout << " xmlns";
+    if (!prefix.empty()) {
+        std::cout << ":" << prefix;
+    }
+    std::cout << "=\"" << uri << "\"";
+}
+
+// XML Comment Handler
+void IdentityParser::handleXMLComment(std::string_view value) {
+    std::cout << "<!--" << value << "-->" << std::endl;
+}
+
+// CDATA Handler
+void IdentityParser::handleCDATA(std::string_view characters) {
+    std::cout << "<![CDATA[" << escape(characters) << "]]>";
+
+    loc += static_cast<int>(std::count(characters.cbegin(), characters.cend(), '\n'));
+}
+
+// processing Instruction Handler
+void IdentityParser::handleProcessingInstruction(std::string_view target, std::string_view data) {
+    std::cout << "<?" << target << " " << data << "?>" << std::endl;
+}
+
+// end Document Handler
+void IdentityParser::handleEndDocument() {}
--- identity.cpp
+++ identity.cpp
@@ -23,6 +23,7 @@
 #include <cassert>
 #include "refillContent.hpp"
 #include "XMLParser.hpp"
+#include "IdentityParser.hpp"
 
 // provides literal string operator""sv
 using namespace std::literals::string_view_literals;
@@ -30,152 +31,17 @@ using namespace std::literals::string_view_literals;
 constexpr auto WHITESPACE = " \n\t\r"sv;
 [[maybe_unused]] constexpr auto NAMEEND = "> /\":=\n\t\r"sv;
 
-std::string escape(std::string_view unescaped) {
-    std::string unescapedString(unescaped);
-    std::string escapedString = "";
-    for(auto c: unescapedString) {
-        if(c == '<') {
-            escapedString += "&lt;";
-        } else if (c == '>') {
-            escapedString += "&gt;";
-        } else if (c == '&') {
-            escapedString += "&amp;";
-        } else {
-            escapedString += c;
-        }
-    }
-    return escapedString;
-}
-
 int main() {
     const auto startTime = std::chrono::steady_clock::now();
-    int unitCount = 0;
-    int loc = 0;
-    int unclosedBrackets = 0;
     std::string_view content;
-    std::string url;
-    XMLParser parser = XMLParser(content);
-
-    const auto XMLDeclarationHandler =
-        [&](std::string_view version, std::optional<std::string_view>& encoding, std::optional<std::string_view>& standalone)->void {
-            std::cout << "<?xml version=\"" << version << "\" ";
-
-            if (encoding.has_value()) {
-                std::cout << "encoding=\"" << encoding.value() << "\" ";
-            }
-
-            if (standalone.has_value()) {
-                std::cout << "standalone=\"" << standalone.value() << "\"";
-            }
-
-            std::cout << "?>" << std::endl;
-         };
-
-    const auto startTagHandler =
-    [&](std::string_view qName, std::string_view prefix, std::string_view localName)->void {
-        if (unclosedBrackets > 0) {
-                --unclosedBrackets;
-                std::cout << ">";
-        }
-        std::cout << "<" << qName;
-        ++unclosedBrackets;
-        if (localName == "unit"sv) {
-            ++unitCount;
-        }
-    };
-
-    const auto endTagHandler =
-        [&](std::string_view qName, std::string_view prefix, std::string_view localName)->void {
-            std::cout << "</" <<  qName << ">";
-         };
-
-    const auto characterHandler =
-    [&](std::string_view characters)->void {
-        if (unclosedBrackets > 0) {
-                --unclosedBrackets;
-                std::cout << ">";
-        }
-        std::cout << escape(characters);
-
-        loc += static_cast<int>(std::count(characters.cbegin(), characters.cend(), '\n'));
-    };
-
-    const auto attributeHandler =
-    [&](std::string_view qName, std::string_view prefix, std::string_view localName, std::string_view value)->void {
-        std::cout << " " << qName << "=\"" << value << "\"";
-
-        if (localName == "url"sv) {
-            url = value;
-        }
-    };
-
-    const auto XMLNamespaceHandler =
-        [&](std::string_view prefix, std::string_view uri)->void {
-            std::cout << " xmlns";
-            if (!prefix.empty()) {
-                std::cout << ":" << prefix;
-            }
-            std::cout << "=\"" << uri << "\"";
-         };
-
-    const auto XMLCommentHandler =
-        [&](std::string_view value)->void {
-            std::cout << "<!--" << value << "-->" << std::endl;
-        };
-
-    const auto CDATAHandler =
-    [&](std::string_view characters)->void {
-        std::cout << "<![CDATA[" << escape(characters) << "]]>";
-
-        loc += static_cast<int>(std::count(characters.cbegin(), characters.cend(), '\n'));
-    };
-
-    const auto processingInstructionHandler =
-        [&](std::string_view target, std::string_view data)->void {
-            std::cout << "<?" << target << " " << data << "?>" << std::endl;
-         };
+    IdentityParser parser = IdentityParser(content);
 
     // parse XML
-    parser.parse(
-
-        // null Start Document handler
-        nullptr,
-
-        // null XML declaration handler
-        XMLDeclarationHandler,
-
-        // start tag handler
-        startTagHandler,
-
-        // null End tag handler
-        endTagHandler,
-
-        // character non-entity references handler
-        characterHandler,
-
-        // attribute handler
-        attributeHandler,
-
-        // null XML namespace handler
-        XMLNamespaceHandler,
-
-        // null XML Comment handler
-        XMLCommentHandler,
-
-        // CDATA handler
-        CDATAHandler,
-
-        // null processing instruction handler
-        processingInstructionHandler,
-
-        // null end document handler
-        nullptr
-
-        );
+    parser.parse();
 
     const auto finishTime = std::chrono::steady_clock::now();
     const auto elapsedSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(finishTime - startTime).count();
-    const auto MLOCPerSecond = loc / elapsedSeconds / 1000000;
+    const auto MLOCPerSecond = parser.getLoc() / elapsedSeconds / 1000000;
     std::cout.imbue(std::locale{""});
     std::clog.imbue(std::locale{""});
     std::clog.precision(3);
